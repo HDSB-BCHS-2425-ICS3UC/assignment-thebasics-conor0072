7447422f9d61c1ee49345cb3d5592069e722c860
conor0072 <1kellycon3@hdsb.ca>
2025-02-24 20:11:56 -0500

    Variable types and print command added

--- firstassignment.py
+++ firstassignment.py
@@ -2,3 +2,18 @@
 # Date: February 24, 2025  
 # This program uses and or does variables, math operations, discriminant calculation, and 3D volume calculations
 
+# Variable Types
+integer = 10  
+float = 10.5  
+double = 10.5555555555
+char = 'A'
+boolean = True  
+string = "Hello"  
+
+# Print the values with their explanations
+print("Integer Variable: " + str(integer))
+print("Float Variable: " + str(float))
+print("Double Variable: " + str(double))
+print("Char Variable: " + char)
+print("Boolean Variable: " + str(boolean))
+print("String Variable: " + string)
