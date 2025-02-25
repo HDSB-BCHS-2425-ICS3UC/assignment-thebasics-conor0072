3016f4f1b68eda1d34b79af271448754a48c6295
conor0072 <1kellycon3@hdsb.ca>
2025-02-25 14:20:53 -0500

    Math Operations Added

--- firstassignment.py
+++ firstassignment.py
@@ -1,6 +1,8 @@
 # Author: Conor Kelly
-# Date: February 24, 2025  
-# This program uses and or does variables, math operations, discriminant calculation, and 3D volume calculations
+# Date: February 25, 2025  
+# This program uses variables, math operations, discriminant calculations, and 3D volume calculations
+
+import math
 
 # Variable Types
 integer = 10  
@@ -11,9 +13,36 @@ boolean = True
 string = "Hello"  
 
 # Print the values with their explanations
-print("Integer Variable: " + str(integer))
-print("Float Variable: " + str(float))
-print("Double Variable: " + str(double))
-print("Char Variable: " + char)
-print("Boolean Variable: " + str(boolean))
-print("String Variable: " + string)
+print("This is an example of a Integer Variable: " + str(integer))
+print("This is an example of a Float Variable: " + str(float))
+print("This is an example of a Double Variable: " + str(double))
+print("This is an example of a Char Variable: " + char)
+print("This is an example of a Boolean Variable: " + str(boolean))
+print("This is an example of a String Variable: " + string)
+
+# Math Operations
+
+add = 2 + 23
+difference = 2 - 23
+multiply = 3 * 2 * 4
+divide = 24 / 8
+divide_float = 24.0 / 7  
+square = math.sqrt(25)
+power = 2 ** 4
+rem_1 = 24 % 6
+rem_2 = 24 % 5
+rem_3 = 24 % 7
+
+# Printing the results
+print("")
+print("Math Operations Stored in Variables:")
+print("Addition (2 + 23): " + str(add))
+print("Subtraction (2 - 23): " + str(difference))
+print("Product (3 * 2 * 4): " + str(multiply))
+print("Quotient Integer (24 / 8): " + str(divide))
+print("Quotient Float (24.0 / 7): " + str(divide_float))
+print("Square Root (math.sqrt(25)): " + str(square))
+print("Exponentiation (2 ** 4): " + str(power))
+print("Modulus 1 (24 % 6): " + str(rem_1))
+print("Modulus 2 (24 % 5): " + str(rem_2))
+print("Modulus 3 (24 % 7): " + str(rem_3))
