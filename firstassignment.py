bc6844677f4d9723a85a11c4418f8f5d98fdd597
conor0072 <1kellycon3@hdsb.ca>
2025-02-26 14:18:11 -0500

    Polish
    Changes made to the print style. 3D volume calculator uses the power function.

--- firstassignment.py
+++ firstassignment.py
@@ -15,14 +15,13 @@ string = "Hello"
 
 # Print the values with their explanations
 
-print("")
-print("Variable Type Examples")
-print("This is an example of a Integer Variable: " + str(integer))
-print("This is an example of a Float Variable: " + str(float))
-print("This is an example of a Double Variable: " + str(double))
-print("This is an example of a Char Variable: " + char)
-print("This is an example of a Boolean Variable: " + str(boolean))
-print("This is an example of a String Variable: " + string)
+print("\nVariable Type Examples")
+print("\nThis is an example of a Integer Variable:", str(integer))
+print("This is an example of a Float Variable:", str(float))
+print("This is an example of a Double Variable:", str(double))
+print("This is an example of a Char Variable:", char)
+print("This is an example of a Boolean Variable:", str(boolean))
+print("This is an example of a String Variable:", string)
 
 # Math Operations
 
@@ -39,23 +38,21 @@ rem_3 = 24 % 7
 
 # Printing the results
 
-print("")
-print("Math Operations Stored in Variables:")
-print("Addition (2 + 23): " + str(add))
-print("Subtraction (2 - 23): " + str(difference))
-print("Product (3 * 2 * 4): " + str(multiply))
-print("Quotient Integer (24 / 8): " + str(divide))
-print("Quotient Float (24.0 / 7): " + str(divide_float))
-print("Square Root (math.sqrt(25)): " + str(square))
-print("Exponentiation (2 ** 4): " + str(power))
-print("Modulus 1 (24 % 6): " + str(rem_1))
-print("Modulus 2 (24 % 5): " + str(rem_2))
-print("Modulus 3 (24 % 7): " + str(rem_3))
+print("\nMath Operations Stored in Variables:")
+print("\nAddition (2 + 23):", str(add))
+print("Subtraction (2 - 23):", str(difference))
+print("Product (3 * 2 * 4):", str(multiply))
+print("Quotient Integer (24 / 8):", str(divide))
+print("Quotient Float (24.0 / 7):", str(divide_float))
+print("Square Root (math.sqrt(25)):", str(square))
+print("Exponentiation (2 ** 4):", str(power))
+print("Modulus 1 (24 % 6):", str(rem_1))
+print("Modulus 2 (24 % 5):", str(rem_2))
+print("Modulus 3 (24 % 7):", str(rem_3))
 
 # Discriminant Calculator 
 
-print("")
-print("Discriminant Calculator")
+print("\nDiscriminant Calculator")
 
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
@@ -63,29 +60,29 @@ c = int(input("Enter c: "))
 
 delta = b * b - 4 * a * c
 
-print("Discriminant:", delta)
+print("\nDiscriminant:", delta)
 
 # 3D Volume Calculator
 
 # Cube Calculator
-print()
-side = int(input("Enter the side length of the cube: "))
-cube_volume = side * side * side
+
+side = int(input("\nEnter the side length of the cube: "))
+cube_volume = side ** 3
 print("Volume of the cube:", cube_volume)
 
 # Sphere Calculator
 radius = int(input("Enter the radius of the sphere: "))
-sphere_volume = (4/3) * 3.14159 * radius * radius * radius
+sphere_volume = (4/3) * 3.14159 * radius ** 3
 print("Volume of the sphere:", sphere_volume)
 
 # Cone Calculator
 radius = int(input("Enter the radius of the cone: "))
 height = int(input("Enter the height of the cone: "))
-cone_volume = (1/3) * 3.14159 * radius * radius * height
+cone_volume = (1/3) * 3.14159 * radius ** 2 * height
 print("Volume of the cone:", cone_volume)
 
 # Cylinder Calculator
 radius = int(input("Enter the radius of the cylinder: "))
 height = int(input("Enter the height of the cylinder: "))
-cylinder_volume = 3.14159 * radius * radius * height
+cylinder_volume = 3.14159 * radius ** 2 * height
 print("Volume of the cylinder:", cylinder_volume)
